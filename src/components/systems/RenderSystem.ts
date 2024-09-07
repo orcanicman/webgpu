43f9112250dbebecbbde8ea53649bf4c0accd851
orcanicman <orcanicman@gmail.com>
2024-09-07 18:03:33 +0200

    restructured files

--- src/components/systems/RenderSystem.ts
+++ src/components/systems/RenderSystem.ts
@@ -205,16 +205,16 @@ export class Content {
 
 	public static async initialize(device: GPUDevice) {
 		this.textures = {
-			playerTexture: await Texture.createTextureFromURL(device, "/src/assets/transparent_16x16.png"),
-			redHitbox: await Texture.createTextureFromURL(device, "/src/assets/red_hitbox.png"),
-			redTexture: await Texture.createTextureFromURL(device, "/src/assets/red_16x32.png"),
-			uvTestTexture: await Texture.createTextureFromURL(device, "/src/assets/uv_test.png"),
+			playerTexture: await Texture.createTextureFromURL(device, "/public/assets/transparent_16x16.png"),
+			redHitbox: await Texture.createTextureFromURL(device, "/public/assets/red_hitbox.png"),
+			redTexture: await Texture.createTextureFromURL(device, "/public/assets/red_16x32.png"),
+			uvTestTexture: await Texture.createTextureFromURL(device, "/public/assets/uv_test.png"),
 		};
 
 		this.animationSheets = {
 			playerAnimationSheet: await Texture.createTextureFromURL(
 				device,
-				"/src/assets/Prototype-character/AnimationSheet.png",
+				"/public/assets/Prototype-character/AnimationSheet.png",
 			),
 		};
 	}
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,42 +3,42 @@ import webpack from "webpack";
 import ESLintPlugin from "eslint-webpack-plugin";
 
 const config: webpack.Configuration = {
-  watch: true,
-  mode: "production",
-  entry: "./src/main.ts",
-  plugins: [
-    new ESLintPlugin({
-      context: "../",
-      failOnError: false,
-      extensions: ["ts"],
-      configType: "flat",
-      eslintPath: "eslint/use-at-your-own-risk",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.wgsl/,
-        loader: "webpack-wgsl-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
+	watch: true,
+	mode: "production",
+	entry: "./src/main.ts",
+	plugins: [
+		new ESLintPlugin({
+			context: "../",
+			failOnError: false,
+			extensions: ["ts"],
+			configType: "flat",
+			eslintPath: "eslint/use-at-your-own-risk",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.wgsl/,
+				loader: "webpack-wgsl-loader",
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts"],
+	},
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: "main.js",
+	},
 };
 
 export default config;
